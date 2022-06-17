068671e55b006f01919a3b58013b8f666cd1f3e2
Jayze24 <32419237+Jayze24@users.noreply.github.com>
2022-06-17 13:06:32 +0900

    Update VisualTransformationFormatter.kt
    add how to use

--- utils/VisualTransformationFormatter.kt
+++ utils/VisualTransformationFormatter.kt
@@ -1,10 +1,26 @@
-package kr.co.itcha.linker.utils
+package compose.utils
 
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.input.OffsetMapping
 import androidx.compose.ui.text.input.TransformedText
 import androidx.compose.ui.text.input.VisualTransformation
-import kr.co.itcha.linker.module.log.Log
+
+/**
+* made by Yu JaeHa
+* ----------
+* HOW TO USE
+* ----------
+*  example)
+*   TextField(..., visualTransformation = VisualTransformationFormatString(digit = listOf(3, 4, 4), stringAppend = " - "), ..)
+*
+*  parameter)
+*   digit : string format rules. 
+*           For example, in "lisOf(3)", the specified character is entered for every 3rd character. like this "123 - 456 - 789 - 01"
+*           if you set like this "listOf(2,1,5)", it will be like this "12 - 3 - 45678 - 90 - 1"
+*   stringAppend : Characters to be inserted between strings.
+*   isReversed : if you set "true", it will count from behind.
+*   max : max return value.
+**/
 
 class VisualTransformationFormatString(
     private val digit: List<Int> = listOf(4),
@@ -57,7 +73,6 @@ class VisualTransformationFormatString(
 
     override fun filter(text: AnnotatedString): TransformedText {
         var value = if (text.text.length > max) text.text.substring(0 until max) else text.text
-        Log.send(value)
         val out = StringBuilder()
         var indexDigit = 0
         var count = 0
@@ -82,7 +97,6 @@ class VisualTransformationFormatString(
         } else {
             out.toString()
         }
-        Log.send("out : $valueFormatted")
 
         return TransformedText(AnnotatedString(valueFormatted), creditCardOffsetTranslator)
     }
